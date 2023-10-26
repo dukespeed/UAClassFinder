03357322d403ee22785170834fb9c5b4a8304d75
Amon <amonguinan@arizona.edu>
2023-10-26 10:48:08 -0700

    added some parse code to webscraper.py

--- websraper.py
+++ websraper.py
@@ -22,3 +22,20 @@ with requests.Session() as s:
     cs120_sessions = s.post(class_search_url,data=search_select_cs)
 
     cs = cs120_sessions.text
+
+    #quick edit to see what we get from the request - Sophie
+    #i should probably move this into its own file...
+    #n.b. the request format makes me want to explode
+    i = 0
+    for line in cs.split(","): 
+        #filter for data for database fields. will be put into database code when that's written.
+        fields_we_want = ["instructor", "daysTimes", "label"] 
+        if "{" in line:
+            #new section
+            print(f"### Section {i} ###")
+            if any([field in line for field in fields_we_want]):
+                #if any of the fields appear in the current line
+                print(line)
+                pass
+            i += 1
+        #print(line)
