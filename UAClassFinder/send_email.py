e9e8d91504b9a960808f232524dae976091482b0
Yash-Agarwal-1 <77954512+Yash-Agarwal-1@users.noreply.github.com>
2023-11-27 23:41:22 -0700

    Experimenting with email

--- UAClassFinder/send_email.py
+++ UAClassFinder/send_email.py
@@ -1,12 +1,20 @@
 from django.core import mail
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UAClassFinder.settings")
-with mail.get_connection() as connection:
-    mail.EmailMessage(
-        "test",
-        "this email was sent automatically. :3",
-        to = ["sophieguinan@arizona.edu", "dukeospeed@arizona.edu"],
-        from_email = "alert@uaclassfinder.co",
-        connection=connection,
-    ).send(fail_silently=False)
-print("worked?")
+# with mail.get_connection() as connection:
+#     mail.EmailMessage(
+#         "test",
+#         "this email was sent automatically. :3",
+#         to = ["sophieguinan@arizona.edu", "dukeospeed@arizona.edu"],
+#         from_email = "alert@uaclassfinder.co",
+#         connection=connection,
+#     ).send(fail_silently=False)
+# print("worked?")
+
+mail.send_mail(
+    'Subject here',
+    'Here is the message.',
+    'alert@uaclassfinder.co',
+    ['2002agarwalyash@gmail.com'],
+    fail_silently=False,
+)
